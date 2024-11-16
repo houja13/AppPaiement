896fdab4040a15a2695d740e9ad63d5f9875ac6b
Hajar <300138576@monboreal.ca>
2024-11-16 18:32:48 -0500

    creation de paypal

---
+++ ConsoleAppPaiement/ConsoleAppPaiement/Paypal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiement
+{
+    internal class Paypal : Paiement
+    {
+        public string Courriel {  get; set; }
+        public Paypal(double montant, string description, string courriel) : base(double montant, string description) 
+        {
+            Courriel = courriel;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le montant {Montant}, la description {Description} et la courriel {Courriel}");
+        }
+    }
+}
--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -5,7 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {
         CarteCredit carteCredit1 = new CarteCredit(5000, "shoping", 1234);
-        CarteCredit carteCredit2 = new CarteCredit(4000, "shoping", 5678);
-        CarteCredit carteCredit3 = new CarteCredit(3000, "shoping", 9012);
+        CarteCredit carteCredit2 = new CarteCredit(4000, "makeup", 5678);
+        CarteCredit carteCredit3 = new CarteCredit(3000, "achat", 9012);
+
+        Paypal paypal1 = new Paypal(500, "vendre", "hajar@gmail.com");
+        Paypal paypal2 = new Paypal(400, "donnation", "nour@gmail.com");
     }
 }
