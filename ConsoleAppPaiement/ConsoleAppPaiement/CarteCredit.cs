dd580dc07f3568e8d54b30431a9c6cd5e59d0224
Hajar <300138576@monboreal.ca>
2024-11-16 18:21:08 -0500

    creation de cartecredit

---
+++ ConsoleAppPaiement/ConsoleAppPaiement/CarteCredit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiement
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+        
+        public CarteCredit(double montant, string description,int cartecredi) : base(double montant, string description)
+        {
+            NumeroCarte = cartecredi;
+        }
+        public override void AfficherDetails() 
+        {
+            Console.WriteLine($"le montant {Montant}, la description {Description} et la carte {NumeroCarte}");
+        }
+    }
+}
--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using ConsoleAppPaiement;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit carteCredit1 = new CarteCredit(5000, "shoping", 1234);
+        CarteCredit carteCredit2 = new CarteCredit(4000, "shoping", 5678);
+        CarteCredit carteCredit3 = new CarteCredit(3000, "shoping", 9012);
     }
 }
