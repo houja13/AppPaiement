fa91913b684b0c174a9828125a1364b9dbdf0c17
Hajar <300138576@monboreal.ca>
2024-12-07 20:11:29 -0500

    instances des class

--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -4,23 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        CarteCredit carteCredit1 = new CarteCredit(500,"shoping",12345);
-        CarteCredit carteCredit2 = new CarteCredit(400, "SPA", 12367);
-        CarteCredit carteCredit3 = new CarteCredit(300, "Restaut", 12389);
+        CarteCredit carteCreditH = new CarteCredit(500,"shoping",12345);
+        CarteCredit carteCreditN = new CarteCredit(400, "SPA", 12367);
+        CarteCredit carteCreditJ = new CarteCredit(300, "Restaut", 12389);
 
 
-        Paypal paypal1 = new Paypal(2000,"Rent","hajar@gmail.com");
-        Paypal paypal2 = new Paypal(4000,"Loisir","Nour@hotmail.com");
+        Paypal paypalH = new Paypal(2000,"Rent","hajar@gmail.com");
+        Paypal paypalN = new Paypal(4000,"Loisir","Nour@hotmail.com");
 
 
 
         Utilisateur utilisateur = new Utilisateur("Hajar");
 
-        utilisateur.AjouterPaiement(carteCredit1);
-        utilisateur.AjouterPaiement(carteCredit2);
-        utilisateur.AjouterPaiement(carteCredit3);
-        utilisateur.AjouterPaiement(paypal1);
-        utilisateur.AjouterPaiement(paypal2);
+        utilisateur.AjouterPaiement(carteCreditH);
+        utilisateur.AjouterPaiement(carteCreditN);
+        utilisateur.AjouterPaiement(carteCreditJ);
+        utilisateur.AjouterPaiement(paypalH);
+        utilisateur.AjouterPaiement(paypalN);
 
         utilisateur.AfficherInfos();
     }
