1880ab88a0259a88d634fede4b8690dc8aac06d6
Hajar <300138576@monboreal.ca>
2024-12-07 20:06:44 -0500

    class utilisateur

--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -8,7 +8,20 @@ internal class Program
         CarteCredit carteCredit2 = new CarteCredit(400, "SPA", 12367);
         CarteCredit carteCredit3 = new CarteCredit(300, "Restaut", 12389);
 
+
         Paypal paypal1 = new Paypal(2000,"Rent","hajar@gmail.com");
         Paypal paypal2 = new Paypal(4000,"Loisir","Nour@hotmail.com");
+
+
+
+        Utilisateur utilisateur = new Utilisateur("Hajar");
+
+        utilisateur.AjouterPaiement(carteCredit1);
+        utilisateur.AjouterPaiement(carteCredit2);
+        utilisateur.AjouterPaiement(carteCredit3);
+        utilisateur.AjouterPaiement(paypal1);
+        utilisateur.AjouterPaiement(paypal2);
+
+        utilisateur.AfficherInfos();
     }
 }
---
+++ ConsoleAppPaiement/ConsoleAppPaiement/Utilisateur.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiement
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Utilisateur: {Nom}");
+            Console.WriteLine("Liste des paiements:");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+
+        }
+    }
+}
