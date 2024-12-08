892fb47e3bd27c1a177a6e3317db6e956b3e5dbd
Hajar <300138576@monboreal.ca>
2024-12-07 19:57:19 -0500

    class paypal

---
+++ ConsoleAppPaiement/ConsoleAppPaiement/Paypal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiement
+{
+    internal class Paypal :Paiement
+    {
+        public string Courriel { get; set; }
+
+        public Paypal(double montant, string description, string courriel) : base(montant, description)
+        {
+            Courriel = courriel;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le montant est {Montant}. Description du montant {Description} et le Courriel est {Courriel}.");
+        }
+
+    }
+}
--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -7,5 +7,8 @@ internal class Program
         CarteCredit carteCredit1 = new CarteCredit(500,"shoping",12345);
         CarteCredit carteCredit2 = new CarteCredit(400, "SPA", 12367);
         CarteCredit carteCredit3 = new CarteCredit(300, "Restaut", 12389);
+
+        Paypal paypal1 = new Paypal(2000,"Rent","hajar@gmail.com");
+        Paypal paypal2 = new Paypal(4000,"Loisir","Nour@hotmail.com");
     }
 }
