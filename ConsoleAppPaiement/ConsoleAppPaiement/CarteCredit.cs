0fb5c8fe5b4ef4dc725d2cce5091b84884d9fde4
Hajar <300138576@monboreal.ca>
2024-12-07 19:50:14 -0500

    class carte credit

---
+++ ConsoleAppPaiement/ConsoleAppPaiement/CarteCredit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiement
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int numeroCarte) : base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le montant est {Montant}. Description: {Description}.le numéro de carte est {NumeroCarte}");
+        }
+
+    }
+}
--- ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
+++ ConsoleAppPaiement/ConsoleAppPaiement/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleAppPaiement;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarteCredit carteCredit1 = new CarteCredit(500,"shoping",12345);
+        CarteCredit carteCredit2 = new CarteCredit(400, "SPA", 12367);
+        CarteCredit carteCredit3 = new CarteCredit(300, "Restaut", 12389);
+    }
+}
